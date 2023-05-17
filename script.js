e5e305030aa8ce102cead65566ee85f33c12aa95
salikwani <120441251+salikwani@users.noreply.github.com>
2023-05-17 15:34:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here!
-
+var list = document.getElementById("infi-list");
+var i=0;
+for(;i<10;i++) {
+	list.innerHTML += `<li>Item ${i+1}</li>`;
+} i++;
+list.addEventListener("scroll",() => {
+	if(list.scrollHeight == list.scrollTop + list.clientHeight) {
+		list.innerHTML += `<li>Item ${i}</li>`;
+		i++;
+		list.innerHTML += `<li>Item ${i}</li>`;
+		i++;
+	}
+});
